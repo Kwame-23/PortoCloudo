46c2cfbbfbb46a8aa000a3a123c3ecb9221077c8
kwakuNti <159504184+kwakuNti@users.noreply.github.com>
2024-11-02 17:37:01 +0000

    Refactor CSS and HTML files

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-// Select all name cards
-document.querySelectorAll('.name-card').forEach(card => {
-    // Add a click event listener to each card
-    card.addEventListener('click', () => {
-        // Redirect to the URL specified in the data-url attribute
-        window.location.href = card.getAttribute('data-url');
-    });
-});
